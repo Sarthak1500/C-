7621d9f17bfcdf4ac676aaa39d2e56c44f877648
Sarthak Kushwaha <82011128+Sarthak1500@users.noreply.github.com>
2025-04-07 16:20:04 +0530

    commit

---
+++ binarysearch/peakelement.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int peakelement(vector<int>nums){
+    int n = nums.size();
+    if(n==1 || nums[0]>nums[1]) return nums[0];
+    for(int i=1;i<n-1;i++){
+        if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) return nums[i];
+    }
+    if(nums[n-1]>nums[n-2])
+     return nums[n-1];
+     return -1;
+        
+
+}
+int main(){
+    vector<int> nums = {1,2,3,4,5,6,7,8,9};
+    cout<<peakelement(nums)<<endl;
+    return 0;
+}
+    
